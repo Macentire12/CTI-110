887a7ff5d796948874bf4c2c0dc1e70ba6bf4e77
Macentire12 <146122143+Macentire12@users.noreply.github.com>
2023-10-05 13:37:00 -0400

    Add files via upload

---
+++ P2HW1_MalachiLewis.py
@@ -0,0 +1,12 @@
+#Malachi Lewis
+#10/5/2023
+#Make a program to gather info about a person
+
+
+name=input("Enter first Name: ")
+hair  =input("Enter hair Color: ")
+eye  =input("Enter eye Color: ")
+height =input("Enter height in feet:")
+age =input("Enter  age:")
+food =input("Enter favorite food: ")
+print(f"{name} is a {height} tall student with {hair} hair and {eye} eyes. They are {age} years old and their favorite food is {food}.")
